1aac20eb50be7e1cab3d75b4a396bd3483381a7d
BarPus <katerinalove02@mail.ru>
2022-06-06 16:32:53 +0700

    Добавьте файлы проекта.

---
+++ VisualFinal/Models/Database/Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisualFinal.Models.Database
+{
+    public partial class Result
+    {
+        public string HorseName { get; set; } = null!;
+        public long RaceNumber { get; set; }
+        public string? Backlog { get; set; }
+        public string? AgeWeight { get; set; }
+
+        public virtual Horse HorseNameNavigation { get; set; } = null!;
+        public virtual Race RaceNumberNavigation { get; set; } = null!;
+    }
+}
---
+++ VisualFinal/Models/StaticTabs/BidTab.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VisualFinal.Models.Database;
+
+namespace VisualFinal.Models.StaticTabs
+{
+    public class BidTab : StaticTab
+    {
+        public BidTab(string h = "", DbSet<Bid>? dBS = null) : base(h)
+        {
+            DBS = dBS;
+            DataColumns = new List<string>();
+            DataColumns.Add("RaceNumber");
+            DataColumns.Add("HorseName");
+            DataColumns.Add("BidderName");
+            DataColumns.Add("Incom");
+            ObjectList = DBS.ToList<object>();
+        }
+
+        new public DbSet<Bid>? DBS { get; set; }
+    }
+}
